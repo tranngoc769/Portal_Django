98975a74d9331f707915c77768882fcffcd74437
Quang <hocmai6@gmail.com>
2020-12-07 00:11:54 +0700

    xu li : dang ki, dang nhap

---
+++ portal/views.py
@@ -0,0 +1,76 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from portal.models import NGUOIDUNG
+from django.views.decorators.csrf import csrf_protect,csrf_exempt
+from portal.models import  *
+import  hashlib
+import json
+def kiemTraCookie(request):
+      print(request)
+      
+# Create your views here.
+# def dangnhap(request):
+#       test = NGUOIDUNG()
+#       data = NGUOIDUNG.objects.all()
+#       print(data)
+#       if request.method =="GET":
+#             return render(request,"portal/dangnhap.html")
+#       else:
+#             return HttpResponse("Method not permission")
+def index(request):
+      request.session['user_id'] = 12
+      if request.method =="GET":
+            return HttpResponse(request.session.get('user_id'))
+      else:
+            return HttpResponse("Method not permission")
+# Router /dangnhap
+@csrf_exempt #Tránh lỗi--CSRF token missing or incorrect
+def dangnhap(request):
+      if request.method =="GET":
+            return render(request,'portal/dangnhap.html')
+      if request.method == "POST":
+            thongTinDN = json.loads(request.body) #Lấy dữ liệu đăng nhập dưới dạng Objects
+            tenDangNhap = thongTinDN['TenDangNhap']
+            matKhau = thongTinDN['MatKhau']
+            # 
+            maHoaMatKhau = hashlib.md5(matKhau.encode()).hexdigest() # Mã hóa mật khẩu md5
+            resp = {"code" : 200, "msg" : "Đăng nhập thành công"} # Biến kết quả (Json), mặc định là thành công, xuống dưới kiểm tra có lỗi thì update, khỏi phải tạo biến mới
+            try:
+                  query_NguoiDung = NGUOIDUNG.objects.filter(TenNguoiDung = tenDangNhap, MatKhau = maHoaMatKhau)
+                  if (len(query_NguoiDung) < 1): #Bé hơn 1 là ko có tài khoản có TenNguoiDung và MatKhau
+                        resp["code"] = 404
+                        resp['msg'] = "Tài khoản hoặc mật khẩu không đúng"
+                  else:
+                        # Thành công --> gán session 
+                        request.session['DaDangNhap'] =  True
+                        request.session['TenDangNhap'] = tenDangNhap
+                        request.session['Quyen'] = query_NguoiDung[0].Quyen # [0] vì chỉ có 1 nguoidung duy nhất (theo TenDangNhap)
+                        
+            except Exception as query_erro:
+                  #   Có lỗi --> không có tài khoản 
+                  resp["code"] = 404
+                  resp['msg'] = str(query_erro)
+            # Trả về kết quả json (nếu không có lỗi gì+query có thông tin thì resp mặc định như khi khai báo do không bị update, ngược lại thì có code 404 và msg theo 2 trường hợp kia)
+            return HttpResponse(json.dumps(resp, ensure_ascii=False))
+# Router /dangki
+@csrf_exempt #Tránh lỗi--CSRF token missing or incorrect
+def dangki(request):
+      if request.method =="GET":
+            return render(request,'portal/dangki.html')
+      if request.method == "POST":
+            thongTinDK = json.loads(request.body) #Lấy dữ liệu đăng ký dưới dạng Objects
+            # 'SDT': '123', 'Email': '1234@gmail.com', 'GioiTinh': '0', 'NgaySinh': '2020-12-10', 'MatKhau': 'password', 'Quyen': 3}
+            maHoaMatKhau = hashlib.md5(thongTinDK.get('MatKhau').encode()).hexdigest() # Mã hóa mật khẩu md5
+            # Các thao tác validate ở đây
+            try:
+                  nguoiDungDk = NGUOIDUNG(TenNguoiDung = thongTinDK.get('TenNguoiDung'), HoTen= thongTinDK.get('HoTen'),SDT = thongTinDK.get('SDT'), MatKhau= maHoaMatKhau,Email = thongTinDK.get('Email'), NgaySinh= thongTinDK.get('NgaySinh'), GioiTinh= thongTinDK.get('GioiTinh'),Quyen = thongTinDK.get('Quyen'))
+                  nguoiDungDk.save()
+            except Exception as insertErr:
+                  resp = {"code" : 404}
+                  resp['msg'] = str(insertErr)
+                  return HttpResponse(json.dumps(resp))
+            resp = {"code" : 200}
+            resp['msg'] = "success"
+            return HttpResponse(json.dumps(resp))
+
+# DANG KY
